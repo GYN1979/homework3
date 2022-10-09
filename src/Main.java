3f29bec96088128ffa1858f84c277271d34797da
GYN1979 <galeeva.y@yandex.ru>
2022-10-09 22:15:51 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,83 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Задача 1
+        System.out.println ("Задача 1");
+        double dog = 8;
+        System.out.println (dog);
+        double cat = 3.6;
+        System.out.println (cat);
+        var paper = 763789;
+        System.out.println (paper);
+
+        // Задача 2
+        System.out.println ("Задача 2");
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println (dog);
+        System.out.println (cat);
+        System.out.println (paper);
+
+        // Задача 3
+        System.out.println ("Задача 3");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println (dog);
+        System.out.println (cat);
+        System.out.println (paper);
+
+        // Задача 4
+        System.out.println ("Задача 4");
+        var friend = 19;
+        friend = friend + 2;
+        System.out.println (friend);
+        friend = friend / 7;
+        System.out.println (friend);
+
+        // Задача 5
+        System.out.println ("Задача 5");
+        var frog = 3.5;
+        System.out.println (frog);
+        frog = frog * 10;
+        System.out.println (frog);
+        frog = frog / 3.5;
+        System.out.println (frog);
+        frog = frog + 4;
+        System.out.println (frog);
+
+        // Задача 6
+        System.out.println ("Задача 6");
+        var weight1 = 78.2;
+        var weight2 = 82.7;
+
+        // Задача 7
+        System.out.println ("Задача 7");
+        var difference = weight2 - weight1;
+        System.out.println ("Разница в весе " + difference + " кг");
+        var difference1 = weight2 % weight1;
+        System.out.println ("Разница в весе " + difference1 + " кг");
+
+        // Задача 8
+        System.out.println ("Задача 8");
+        var totalHours = 640;
+        var oneEmployee = 8;
+        var totalEmployee = totalHours / oneEmployee;
+        System.out.println ("Всего работников в компании " + totalEmployee + " человек");
+        totalEmployee = totalEmployee + 94;
+        System.out.println (totalEmployee);
+        var totalHours1 = oneEmployee * totalEmployee;
+        System.out.println ("Если в компании работает  174 человека, то всего " + totalHours1 + " часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
+
+
+    }
+}
